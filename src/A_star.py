630aa446fe436ca53678f47a39ead01df3aba8d4
Chloé Boninn <chloe.boninn@telecomnancy.eu>
2025-04-09 10:40:15 +0200

    fin a_star/correction

--- src/A_star.py
+++ src/A_star.py
@@ -3,16 +3,79 @@ from vector import Vector2
 from constants import *
 import numpy as np
 from constants import *
+from nodes import Node
 
 
 class A_star():
-    def __init__(self,ghosts,pellets,pacman,dt):
+    def __init__(self,ghosts,pellets,pacman,dt,level): #pb: le dt est fixe pour toute la partie
         self.ghosts = ghosts
         self.pellets = pellets
         self.pacman = pacman
         # self.pelletfile="resources/maze1.txt"
         self.dpl=round(pacman.speed*dt,2)
         # print("dpl",self.dpl,dt)
+
+        #création de la map
+        self.level=level
+        self.nodes={}
+        self.pathSymbols = ['.', '-', '|', 'p','+', 'P', 'n']
+        data = self.readMazeFile(level)
+        self.createNodeTable(data)
+        self.connectHorizontally(data)
+        self.connectVertically(data)
+        self.homekey = None
+        # for n in self.nodes:
+        #     neig=[]
+        #     for neighbor in self.nodes[n].neighbors:
+        #         if self.nodes[n].neighbors[neighbor]!=None :
+        #             neig.append((self.nodes[n].neighbors[neighbor].position.x,self.nodes[n].neighbors[neighbor].position.y))
+        #         else:
+        #             neig.append(None)
+        #     print(f"node : {n} : {neig}")
+    
+    def createNodeTable(self, data):
+        for row in list(range(data.shape[0])):
+            for col in list(range(data.shape[1])):
+                if data[row][col] in self.pathSymbols:
+                    x, y = col*TILEWIDTH, row*TILEHEIGHT
+                    self.nodes[(x, y)] = Node(x, y)
+
+    def readMazeFile(self, textfile):
+        return np.loadtxt(textfile, dtype='<U1')
+    
+    def connectHorizontally(self, data, xoffset=0, yoffset=0):
+        for row in list(range(data.shape[0])):
+            key = None
+            for col in list(range(data.shape[1])):
+                if data[row][col] in self.pathSymbols:
+                    if key is None:
+                        key = (col*TILEWIDTH,row*TILEHEIGHT)
+                    else:
+                        otherkey = (col*TILEWIDTH,row*TILEHEIGHT)
+                        self.nodes[key].neighbors[RIGHT] = self.nodes[otherkey]
+                        self.nodes[otherkey].neighbors[LEFT] = self.nodes[key]
+                        key = otherkey
+                else:
+                    key = None
+    
+    def connectVertically(self, data):
+        dataT = data.transpose()
+        for col in list(range(dataT.shape[0])):
+            key = None
+            for row in list(range(dataT.shape[1])):
+                if dataT[col][row] in self.pathSymbols:
+                    if key is None:
+                        key = (col*TILEWIDTH,row*TILEHEIGHT)
+                    else:
+                        otherkey = (col*TILEWIDTH,row*TILEHEIGHT)
+                        self.nodes[key].neighbors[DOWN] = self.nodes[otherkey]
+                        self.nodes[otherkey].neighbors[UP] = self.nodes[key]
+                        key = otherkey
+                else:
+                    key = None
+
+
+
 
     def next_move (self):   # doit renvoyer UP, LEFT, DOWN, RIGHT ou STOP
         # lance a_star sur la grille de jeu avec le bon objectif
@@ -43,14 +106,15 @@ class A_star():
         # start=self.pacman.position
         # start=Vector2(32,64)
         goal=self.get_closest_pellet()
-        path=self.a_star_correct(goal)
+        path=self.a_star(goal)
         # print(f"start : {start}")
         
         if path is None or len(path)==0:
             # print("test")
             return STOP
         else:
-            print(f"path : {[(n.position.x,n.position.y) for n in path]}")
+            print(f"path : {[((n.position.x,n.position.y),n.dir) for n in path]}")
+            print(f"dir voulue voulue : {path[0].dir}")
             return path[0].dir
 
 
@@ -58,74 +122,28 @@ class A_star():
         return self.pellets[0]
         
 
-    # def a_star(self,goal):
-    #     # la position de pacman prend les multiples de 16 les plus proches
-    #     start_16=Vector2(int(self.pacman.position.x/16)*16,int(self.pacman.position.y/16)*16)
-    #     print(f"start_16 : {start_16}")
-    #     start_noeud = Noeud(start_16,0,0) #voir quoi mettre pour h
-    #     start_noeud.position.x=int(start_noeud.position.x)
-    #     start_noeud.position.y=int(start_noeud.position.y)
-    #     open = []
-    #     heapq.heappush(open, start_noeud)
-    #     closed = []
-    #     current=start_noeud
-    #     # goal_find = (open[0].position == goal)
-    #     goal_find = collide_goal(self.pacman,goal)
-    #     while len(open)!=0 and not goal_find:
-    #         open=supp_noeud(open,current.position)
-    #         closed.append(current)
-    #         neighbors = current.findNeighbors(self.pacman,self.ghosts,self.dt) #les voisins initialisés avec g=0 et h=0
-    #         # print(f"neighbors : {[(n.position.x,n.position.y) for n in neighbors]}")
-    #         for neighbor in neighbors:
-    #             if (not appartenir(closed,neighbor.position) and not appartenir(open,neighbor.position)) or (appartenir(open,neighbor.position) and getGNoeud(open,neighbor.position)>current.g+1): # TODO: cout(s,s')=1 : à modifier
-    #                 if appartenir(open,neighbor.position):
-    #                     open=supp_noeud(open,neighbor.position)
-    #                 neighbor.g=current.g+1
-    #                 neighbor.h= heuristic(neighbor,goal.position)
-    #                 neighbor.f=neighbor.g+neighbor.h
-    #                 neighbor.parent=current
-    #                 heapq.heappush(open,neighbor)
-    #                 if appartenir(closed,neighbor.position):
-    #                     closed=supp_noeud(closed,neighbor.position)
-                    
-    #         if len(open)!=0:
-    #             current=open[0]
-    #         # print(f"ope : {[(n.position.x,n.position.y) for n in open]}")
-    #     if len(open)==0:
-    #         return None
-    #     else:
-    #         path=[]
-    #         while current.parent != None:
-    #             path.append(current)
-    #             current=current.parent
-    #         return path[::-1]
-
-    def a_star_correct(self,goal):
-        #TODO : rajouter un nb d'itérations au bout duquel on abandonne la recherche ?
-        start = self.pacman.position
-        print(f"start : {start}")
-        # start=goal.position.copy()
-        start_noeud = Noeud(start,0,0)
-        print(f"heuristique goal-start : {heuristic(start_noeud,goal.position)}")
+    def a_star(self,goal):
+        start_16=Vector2((self.pacman.position.x//16)*16,(self.pacman.position.y//16)*16)
+        print(f"start_16 : {start_16}")
+        start_noeud = Noeud(start_16,0,0) #voir quoi mettre pour h
+        start_noeud.position.x=int(start_noeud.position.x)
+        start_noeud.position.y=int(start_noeud.position.y)
         open = []
         heapq.heappush(open, start_noeud)
         closed = []
         current=start_noeud
-        goal_find = open[0].isGoal(goal,self.pacman) #test si la position du noeud permet de manger le pellet goal
+        # goal_find = (open[0].position == goal)
+        goal_find = open[0].isGoal(goal,self.pacman)
         while len(open)!=0 and not goal_find:
-            # print(f"current : {open[0].position}, f = {current.f}, g= {current.g}, h = {current.h}")
-            # print(f"longueur de open : {len(open)}")
             open=supp_noeud(open,current.position)
-            # print(f"longueur de open après : {len(open)}")
             closed.append(current)
-            # print(f"longueur de close : {len(closed)}\n")
-            neighbors = current.findNeighbors(self.pacman,self.ghosts,self.dpl) #les voisins initialisés avec g=0 et h=0
+            neighbors = current.findNeighbors(self.nodes,self.ghosts) #les voisins initialisés avec g=0 et h=0
             # print(f"neighbors : {[(n.position.x,n.position.y) for n in neighbors]}")
             for neighbor in neighbors:
-                if (not appartenir(closed,neighbor.position) and not appartenir(open,neighbor.position)) or (appartenir(open,neighbor.position) and getGNoeud(open,neighbor.position)>current.g+self.dpl): # TODO: cout(s,s')=1 : à modifier
+                if (not appartenir(closed,neighbor.position) and not appartenir(open,neighbor.position)) or (appartenir(open,neighbor.position) and getGNoeud(open,neighbor.position)>current.g+1): # TODO: cout(s,s')=1 : à modifier
                     if appartenir(open,neighbor.position):
                         open=supp_noeud(open,neighbor.position)
-                    neighbor.g=current.g+self.dpl
+                    neighbor.g=current.g+1
                     neighbor.h= heuristic(neighbor,goal.position)
                     neighbor.f=neighbor.g+neighbor.h
                     neighbor.parent=current
@@ -136,10 +154,7 @@ class A_star():
             if len(open)!=0:
                 current=open[0]
                 goal_find = open[0].isGoal(goal,self.pacman) #test si la position du noeud permet de manger le pellet goal
-            
-            # print(f"dt*speed = {self.dpl}")
-            # b=a
-        # print(len(closed))
+            # print(f"ope : {[(n.position.x,n.position.y) for n in open]}")
         if len(open)==0:
             return None
         else:
@@ -148,6 +163,55 @@ class A_star():
                 path.append(current)
                 current=current.parent
             return path[::-1]
+
+    # def a_star_correct(self,goal):
+    #     #TODO : rajouter un nb d'itérations au bout duquel on abandonne la recherche ?
+    #     start = self.pacman.position
+    #     print(f"start : {start}")
+    #     # start=goal.position.copy()
+    #     start_noeud = Noeud(start,0,0)
+    #     print(f"heuristique goal-start : {heuristic(start_noeud,goal.position)}")
+    #     open = []
+    #     heapq.heappush(open, start_noeud)
+    #     closed = []
+    #     current=start_noeud
+    #     goal_find = open[0].isGoal(goal,self.pacman) #test si la position du noeud permet de manger le pellet goal
+    #     while len(open)!=0 and not goal_find:
+    #         # print(f"current : {open[0].position}, f = {current.f}, g= {current.g}, h = {current.h}")
+    #         # print(f"longueur de open : {len(open)}")
+    #         open=supp_noeud(open,current.position)
+    #         # print(f"longueur de open après : {len(open)}")
+    #         closed.append(current)
+    #         # print(f"longueur de close : {len(closed)}\n")
+    #         neighbors = current.findNeighbors(self.pacman,self.ghosts,self.dpl) #les voisins initialisés avec g=0 et h=0
+    #         # print(f"neighbors : {[(n.position.x,n.position.y) for n in neighbors]}")
+    #         for neighbor in neighbors:
+    #             if (not appartenir(closed,neighbor.position) and not appartenir(open,neighbor.position)) or (appartenir(open,neighbor.position) and getGNoeud(open,neighbor.position)>current.g+self.dpl): # TODO: cout(s,s')=1 : à modifier
+    #                 if appartenir(open,neighbor.position):
+    #                     open=supp_noeud(open,neighbor.position)
+    #                 neighbor.g=current.g+self.dpl
+    #                 neighbor.h= heuristic(neighbor,goal.position)
+    #                 neighbor.f=neighbor.g+neighbor.h
+    #                 neighbor.parent=current
+    #                 heapq.heappush(open,neighbor)
+    #                 if appartenir(closed,neighbor.position):
+    #                     closed=supp_noeud(closed,neighbor.position)
+                    
+    #         if len(open)!=0:
+    #             current=open[0]
+    #             goal_find = open[0].isGoal(goal,self.pacman) #test si la position du noeud permet de manger le pellet goal
+            
+    #         # print(f"dt*speed = {self.dpl}")
+    #         # b=a
+    #     # print(len(closed))
+    #     if len(open)==0:
+    #         return None
+    #     else:
+    #         path=[]
+    #         while current.parent != None:
+    #             path.append(current)
+    #             current=current.parent
+    #         return path[::-1]
 
 class Noeud():
     def __init__(self, position, g=0, h=0,dir=STOP):
@@ -162,18 +226,40 @@ class Noeud():
         """Permet de comparer deux objets Noeud par la valeur de f."""
         return self.f < other.f
     
-    def findNeighbors(self,pacman,ghosts,dpl):
-        directions={STOP:Vector2(), UP:Vector2(0, -1),DOWN:Vector2(0, 1), 
-                          LEFT:Vector2(-1, 0), RIGHT:Vector2(1, 0)}
-        neighbors=[]
-        for dir in [UP,DOWN,LEFT,RIGHT]:
-            # print(f"dir : {dir}")
-            if self.validDirection(dir):
-                # print(f"valid direction : {dir,directions[dir].x,directions[dir].y}")
-                pos=self.position+directions[dir]*dpl
-                if pos.x<=416 and pos.x>=16 and pos.y>=64 and pos.y<=512:
-                    neighbors.append(Noeud(pos))
+    def findNeighbors(self,nodes,ghosts): #TODO : gérer les fantômes
+        # directions={STOP:Vector2(), UP:Vector2(0, -1),DOWN:Vector2(0, 1), 
+        #                   LEFT:Vector2(-1, 0), RIGHT:Vector2(1, 0)}
+        # neighbors=[]
+        # for dir in [UP,DOWN,LEFT,RIGHT]:
+        #     # print(f"dir : {dir}")
+        #     if self.validDirection(dir):
+        #         # print(f"valid direction : {dir,directions[dir].x,directions[dir].y}")
+        #         pos=self.position+directions[dir]*dpl
+        #         if pos.x<=416 and pos.x>=16 and pos.y>=64 and pos.y<=512:
+        #             neighbors.append(Noeud(pos))
         
+        neighbors=[]
+        x,y=(self.position.x,self.position.y)
+        pos=(x,y)
+        for neighbor in nodes[pos].neighbors:
+            if nodes[pos].neighbors[neighbor]!=None :
+                neighbors.append(Noeud(nodes[pos].neighbors[neighbor].position,dir=neighbor))
+        # print(f"node : {pos} : {neighbors}")
+        return neighbors
+
+    # def findNeighbors_correct(self,pacman,ghosts,dpl):
+    #     directions={STOP:Vector2(), UP:Vector2(0, -1),DOWN:Vector2(0, 1), 
+    #                       LEFT:Vector2(-1, 0), RIGHT:Vector2(1, 0)}
+    #     neighbors=[]
+    #     for dir in [UP,DOWN,LEFT,RIGHT]:
+    #         # print(f"dir : {dir}")
+    #         if self.validDirection(dir):
+    #             # print(f"valid direction : {dir,directions[dir].x,directions[dir].y}")
+    #             pos=self.position+directions[dir]*dpl
+    #             if pos.x<=416 and pos.x>=16 and pos.y>=64 and pos.y<=512:
+    #                 neighbors.append(Noeud(pos))
+    
+
         # print(pacman.validDirections())
         # for dir in pacman.validDirections():
         #     print(directions[dir])
@@ -196,10 +282,12 @@ class Noeud():
             # pacman.position=pos_save
             # if pacman.validDirection(dir) and not collideGhosts(pos,ghosts,pacman): #TODO : remplacer validDirection
             #     neighbors.append(Noeud(pos,0,0,dir))
-        return neighbors
+        # return neighbors
     
-    def validDirection(self,dir):
-        if 
+    def isValidDirection(self,dir):
+        #tester si il n'y a pas de mur, si c'est dans le labyrinthe et si il n'y a pas de fantôme
+        #récupérer la case sur laquelle est pacman d'après sa position
+        
         return 
     
     def validDirection(self, direction):
